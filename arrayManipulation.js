eea4d2c641391924b2557ca69e115fa5928401e7
MY PC <paarmah004@st.ug.edu.gh>
2024-05-19 00:00:45 +0000

    Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,11 @@
+function processArray(numbers) {
+    const processedArray = numbers.map(num => {
+        if (num % 2 === 0) {
+            return num * num;
+        } else {
+            return num * 3;
+        }
+    });
+
+    return processedArray;
+}
